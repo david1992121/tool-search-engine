a327648185c75dcc996e8906cc2933dfad388b17
Minome <davidoliver5086@gmail.com>
2021-09-06 13:09:08 +0800

    change search url

--- program/urls.py
+++ program/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', get_programs),
+    path('search', get_programs),
     path('tooling', get_toolings),
     path('tools', get_tools),
     path('pdf', download_pdf)
