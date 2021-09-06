c827d8d07f62a160a993334f16e0032690d2e287
Minome <davidoliver5086@gmail.com>
2021-09-06 13:12:18 +0800

    change settings

--- tooling/settings.py
+++ tooling/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 environ.Env.read_env(".env")
-env.DB_SCHEMES.pop("mssql")
+if "mssql" in env.DB_SCHEMES.keys():
+    env.DB_SCHEMES.pop("mssql")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
